ea72d7c0660c12451fd6632fbb7a7930b7ca8088
Ezequiel Troya <getroya1@espe.edu.ec>
2025-10-31 07:34:10 -0500

    ExtructuraCodigo

---
+++ Codificacion/v1/GESTOR_DE_TAREAS_PERSONALES_V1/src/main/java/com/mycompany/gestor_de_tareas_personales_v1/GESTOR_DE_TAREAS_PERSONALES_V1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestor_de_tareas_personales_v1;
+
+/**
+ *
+ * @author Asus
+ */
+public class GESTOR_DE_TAREAS_PERSONALES_V1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
